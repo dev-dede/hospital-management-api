ac6b97eba3d9baae7b1e786148cb44df4c2c3876
dev-dede <tsewayog@gmail.com>
2025-04-03 04:43:35 +0000

    feat: enable medical record detail view for doctors

--- patient_records/urls.py
+++ patient_records/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.urls import path
 from .views import (
     MedicalRecordCreateView,
+    MedicalRecordListView,
+    MedicalRecordDetailView
 )
 
 urlpatterns = [
-    path('create/', MedicalRecordCreateView.as_view(), name='create-medical-record')
+    path('create/', MedicalRecordCreateView.as_view(), name='create-medical-record'),
+    path('', MedicalRecordListView.as_view(), name='list-medical-record'),
+    path('<int:pk>/', MedicalRecordDetailView.as_view(), name='detail-medical-record')
 ]
--- patient_records/views.py
+++ patient_records/views.py
@@ -2,7 +2,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import (
     CreateAPIView,
     ListAPIView,
-    RetrieveUpdateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
     DestroyAPIView,
 )
 from .serializers import (
@@ -20,3 +21,20 @@ class MedicalRecordCreateView(CreateAPIView):
     serializer_class = MedicalRecordSerializer
     permission_classes = [IsAuthenticated, IsDoctor]
 
+# Only doctors can create medical records
+class MedicalRecordListView(ListAPIView):
+    serializer_class = MedicalRecordSerializer
+    permission_classes = [IsAuthenticated, (IsDoctor | IsPatient)]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == "Doctor":
+            return MedicalRecord.objects.all()
+        if user.role == "Patient":
+            return MedicalRecord.objects.filter(patient=user.patientprofile)
+        
+class MedicalRecordDetailView(RetrieveAPIView):
+    queryset = MedicalRecord.objects.all()
+    serializer_class = MedicalRecordSerializer
+    # Patient get only their medical record with list hence no need for detailview for patients
+    permission_classes = [IsAuthenticated, IsDoctor]
