2f65a43ea32b0a2da63d32f8a845f25418205838
dev-dede <tsewayog@gmail.com>
2025-04-03 05:24:59 +0000

    fix: remove double full name display

--- patient_records/models.py
+++ patient_records/models.py
@@ -14,7 +14,17 @@ class MedicalRecord(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Medical Record for {self.patient.user.get_full_name()} {self.patient.user.get_full_name()}"
+        return f"Medical Record for {self.patient.user.get_full_name()}"
 
+class Diagnosis(models.Model):
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE, related_name="patientdiagnosis")
+    doctor = models.ForeignKey(DoctorProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name="doctordiagnosis")
+    medical_record = models.ForeignKey(MedicalRecord, on_delete=models.CASCADE)
+    added_notes = models.TextField(blank=True, null=True)
+    condition = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"Diagnosis for {self.patient.user.get_full_name()} by Dr. {self.doctor.user.get_full_name()}"
 
