1d5e63e6fc834667b577870d212277fcb0851e94
dev-dede <tsewayog@gmail.com>
2025-04-04 09:18:37 +0000

    feat: add permissions

--- users/permissions.py
+++ users/permissions.py
@@ -27,3 +27,18 @@ class IsAdmin(permissions.BasePermission):
     """
     def has_permission(self, request, view):
         return request.user.role == 'Admin'
+    
+class IsPharmacistOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission: 
+    - Pharmacists can CREATE, UPDATE, DELETE medications.
+    - Doctors & Patients can only READ.
+    """
+
+    def has_permission(self, request, view):
+        # Allow read-only access for GET, HEAD, OPTIONS requests
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Only allow Pharmacists to modify data
+        return request.user.is_authenticated and request.user.role == "pharmacist"
